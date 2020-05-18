cc40d0eea81e0dc751a691837671a37c04633b65
lyric-zemin <2443835100@qq.com>
2020-05-18 10:11:48 +0800

    初始化文档结构，引入vant组件库

---
+++ api/http.js
@@ -0,0 +1,40 @@
+export {
+  API_BASE_URL,
+  OK_STATUS
+}
+from '../config'
+
+export default function http({
+  url,
+  data = {},
+  method = 'GET',
+  header = {}
+}) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: API_BASE_URL + url,
+      method,
+      header,
+      data,
+      success(res) {
+        if (res.status === OK_STATUS) {
+          resolve(res.data)
+        } else {
+          toastErr()
+        }
+      },
+      fail(err) {
+        reject(err)
+        toastErr()
+      }
+    })
+  })
+}
+
+function toastErr(title = '似乎出了一些问题') {
+  wx.showToast({
+    title,
+    icon: 'none',
+    duration: 1500
+  })
+}
---
+++ config.js
@@ -0,0 +1,3 @@
+export const API_BASE_URL = ''
+
+export const OK_STATUS = 200
